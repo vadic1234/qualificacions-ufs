942faa194542e6ec9a20eeb7c4be62a12515cd90
vadic1234 <vadicesteba@inspladelestany.cat>
2022-09-14 18:08:37 +0100

    main.py

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-# This is a sample Python script.
+# Definició de classes
+class UnitatFormativa:
+    nom = None
 
-# Press Mayús+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+    def __init__(self, nom):
+        self.nom = nom
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-#Autor Vàdic Esteba
+# Inici del programa
+uf1 = UnitatFormativa("UF1. Desenvolupament del programari")
+uf2 = UnitatFormativa("UF2. Optimització del programari")
+uf3 = UnitatFormativa("UF3. Introducció al Disseny Orientat a Objectes")
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print(uf1.nom)
+print(uf2.nom)
+print(uf3.nom)
