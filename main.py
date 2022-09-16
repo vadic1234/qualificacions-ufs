173039e5975b46fab47f791cc4476ce0e0e2dbab
janirahuesca-inspladelestany <janirahuesca@inspladelestany.cat>
2022-09-16 17:12:18 +0200

    Refactor

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # Definició de classes
 class UnitatFormativa:
     nom = None
-    nota = None
+    qualificacio = None
 
     def __init__(self, nom):
         self.nom = nom
@@ -12,10 +12,10 @@ uf1 = UnitatFormativa("UF1. Desenvolupament del programari")
 uf2 = UnitatFormativa("UF2. Optimització del programari")
 uf3 = UnitatFormativa("UF3. Introducció al Disseny Orientat a Objectes")
 
-uf1.nota = 8
-uf2.nota = 10
-uf3.nota = 4
+uf1.qualificacio = 8
+uf2.qualificacio = 10
+uf3.qualificacio = 4
 
-print(uf1.nom, ":", uf1.nota)
-print(uf2.nom, ":", uf1.nota)
-print(uf3.nom, ":", uf1.nota)
+print(uf1.nom, ":", uf1.qualificacio)
+print(uf2.nom, ":", uf1.qualificacio)
+print(uf3.nom, ":", uf1.qualificacio)
