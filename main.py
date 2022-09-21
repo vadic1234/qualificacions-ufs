07ca726bf301da7476d8b76935a9ff3692f59a69
vadic1234 <vadicesteba@inspladelestany.cat>
2022-09-21 18:13:54 +0100

    Canvis en el Main

--- main.py
+++ main.py
@@ -1,34 +1,6 @@
 # Definició de classes
-class UnitatFormativa:
-    nom = None
-    qualificacio = None
-    hores = None
-
-    def __init__(self, nom, hores):
-        self.nom = nom
-        self.hores = hores
-
-
-class ModulProfessional:
-    nom = None
-    unitats_formatives = []
-
-    def __init__(self, nom):
-        self.nom = nom
-
-    def afegirUnitatFormativa(self, unitat_formativa):
-        self.unitats_formatives.append(unitat_formativa);
-
-    def getQualificacio(self):
-        suma_hores = 0
-        suma_qualificacio = 0
-
-        for uf in self.unitats_formatives:
-            suma_hores += uf.hores
-            suma_qualificacio += (uf.qualificacio * uf.hores)
-
-        return suma_qualificacio / suma_hores
-
+from Modul_Professional import ModulProfessional
+from Unitats_Formatives import UnitatFormativa
 
 # Inici del programa
 uf1 = UnitatFormativa("UF1. Desenvolupament del programari", 20)
@@ -41,11 +13,11 @@ uf3.qualificacio = 4
 
 mp5 = ModulProfessional("MP05. Entorns de desenvolupament")
 
-mp5.afegirUnitatFormativa(uf1)
-mp5.afegirUnitatFormativa(uf2)
-mp5.afegirUnitatFormativa(uf3)
+mp5.afegir_unitat_formativa(uf1)
+mp5.afegir_unitat_formativa(uf2)
+mp5.afegir_unitat_formativa(uf3)
 
 print(uf1.nom, ":", uf1.qualificacio)
 print(uf2.nom, ":", uf2.qualificacio)
 print(uf3.nom, ":", uf3.qualificacio)
-print(mp5.nom, ":", mp5.getQualificacio())
+print(mp5.nom, ":", mp5.get_qualificacio())
