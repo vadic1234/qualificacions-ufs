38e111720ac1e13e35777e4a7f749e999a69cb20
janirahuesca-inspladelestany <janirahuesca@inspladelestany.cat>
2022-09-23 16:54:08 +0200

    Afegit refactor d'extracció de mètodes i p¡modificat diagrama de classes

--- main.py
+++ main.py
@@ -2,22 +2,26 @@
 from ModulProfessional import ModulProfessional
 from UnitatFormativa import UnitatFormativa
 
-# Inici del programa
-uf1 = UnitatFormativa("UF1. Desenvolupament del programari", 20)
-uf2 = UnitatFormativa("UF2. Optimització del programari", 20)
-uf3 = UnitatFormativa("UF3. Introducció al Disseny Orientat a Objectes", 26)
+def inicialitzar_modul_professional():
+    global uf1, uf2, uf3, mp5
+    # Inici del programa
+    uf1 = UnitatFormativa("UF1. Desenvolupament del programari", 20)
+    uf2 = UnitatFormativa("UF2. Optimització del programari", 20)
+    uf3 = UnitatFormativa("UF3. Introducció al Disseny Orientat a Objectes", 26)
+    uf1.qualificacio = 8
+    uf2.qualificacio = 10
+    uf3.qualificacio = 4
+    mp5 = ModulProfessional("MP05. Entorns de desenvolupament")
+    mp5.afegir_unitat_formativa(uf1)
+    mp5.afegir_unitat_formativa(uf2)
+    mp5.afegir_unitat_formativa(uf3)
 
-uf1.qualificacio = 8
-uf2.qualificacio = 10
-uf3.qualificacio = 4
+def mostrar_modul_professional():
+    print(uf1.nom, ":", uf1.qualificacio)
+    print(uf2.nom, ":", uf2.qualificacio)
+    print(uf3.nom, ":", uf3.qualificacio)
+    print(mp5.nom, ":", mp5.get_qualificacio())
 
-mp5 = ModulProfessional("MP05. Entorns de desenvolupament")
 
-mp5.afegir_unitat_formativa(uf1)
-mp5.afegir_unitat_formativa(uf2)
-mp5.afegir_unitat_formativa(uf3)
-
-print(uf1.nom, ":", uf1.qualificacio)
-print(uf2.nom, ":", uf2.qualificacio)
-print(uf3.nom, ":", uf3.qualificacio)
-print(mp5.nom, ":", mp5.get_qualificacio())
+inicialitzar_modul_professional()
+mostrar_modul_professional()
