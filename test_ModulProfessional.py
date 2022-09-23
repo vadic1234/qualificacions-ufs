4a81bf89b8dba6184ece77a9efbc52fa1f69a3db
janirahuesca-inspladelestany <janirahuesca@inspladelestany.cat>
2022-09-23 17:40:42 +0200

    Afegit test

---
+++ test_ModulProfessional.py
@@ -0,0 +1,16 @@
+from unittest import TestCase
+
+from ModulProfessional import ModulProfessional
+from UnitatFormativa import UnitatFormativa
+
+
+class TestModulProfessional(TestCase):
+    def test_get_qualificacio(self):
+        mp = ModulProfessional("Prova de mòdul professional");
+        uf1 = UnitatFormativa("UF1", 10);
+        uf1.qualificacio = 0
+        uf2 = UnitatFormativa("UF2", 40);
+        uf2.qualificacio = 10
+        mp.afegir_unitat_formativa(uf1)
+        mp.afegir_unitat_formativa(uf2)
+        assert(mp.get_qualificacio() == 8)
